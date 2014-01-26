6202b1acc56a112f0a1a0959028500646e4e7931
snowflip <xuhansheng@gmail.com>
2014-01-25 19:18:11 -0500

    modified:   hello.cpp

--- cplusplus/hello.cpp
+++ cplusplus/hello.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "hello world!" << endl;
+    //cout << "hello world!" << endl;
+    //cout << "(\") (\\)" << endl;
+    char a = 'a';
+    wchar_t b = 'b';
+    cout << "char length is " << sizeof(a) << endl;
+    cout << "wchar_t length is " << sizeof(b) << endl; 
+    //cout << "a\tb" << endl;
     return 0;
 }
 
